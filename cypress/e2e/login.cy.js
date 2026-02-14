f76e28b84ff1848425dee5d03c757208082da513
Renan Novaes <renan.cunha@protonmail.com>
2026-02-14 09:17:31 -0300

    First COmmit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,17 @@
+describe('Orange HRM Tests', () => {
+  it.only('login-success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('.oxd-input').type('Admin')
+    cy.get('[name="password"]').type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+    it('login-Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('[name="username"]').type('test')
+    cy.get('[name="password"]').type('test123')
+    cy.get('.oxd-button').click()
+    cy.get("[role='alert']")
+    })
+  })
