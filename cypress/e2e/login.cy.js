1fbe065487092c3be30b7a08439e9466e2a7a8be
Renan Novaes <renan.cunha@protonmail.com>
2026-02-15 14:28:02 -0300

    First Commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 describe('Orange HRM Tests', () => {
-  it.only('login-success', () => {
+  it('login-success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('.oxd-input').type('Admin')
+    cy.get('[name="username"]').type('Admin')
     cy.get('[name="password"]').type('admin123')
     cy.get("[type='submit']").click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
