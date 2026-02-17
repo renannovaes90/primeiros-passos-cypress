b6bd790e48e3e6dbf034d8b1c1a504b7b1323140
Renan Novaes <renan.cunha@protonmail.com>
2026-02-17 15:06:50 -0300

    new selector for dashboard added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,7 +5,8 @@ describe('Orange HRM Tests', () => {
     passwordField: '[name="password"]',
     loginButton: "[type='submit']",
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    dashBoardGrid: ".orangehrm-dashboard-grid"
 
   }
 
@@ -15,9 +16,9 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorList.passwordField).type('admin123')
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorList.dashBoardGrid)
   })
-    it.only('login-Fail', () => {
+    it('login-Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(selectorList.usernameField).type('test')
     cy.get(selectorList.passwordField).type('test123')
